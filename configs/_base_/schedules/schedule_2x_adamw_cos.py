f8e648de90bd0d4b0934fcd9b5936470f7362c69
Yuki <workcomputer.com>
2023-11-24 10:26:36 +0800

    auto_scale_lr enable=False

--- configs/_base_/schedules/schedule_2x_adamw_cos.py
+++ configs/_base_/schedules/schedule_2x_adamw_cos.py
@@ -46,4 +46,4 @@ param_scheduler = [
 #   - `enable` means enable scaling LR automatically
 #       or not by default.
 #   - `base_batch_size` = (8 GPUs) x (2 samples per GPU).
-auto_scale_lr = dict(enable=True, base_batch_size=16)
+auto_scale_lr = dict(enable=False, base_batch_size=16)
